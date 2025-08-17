7a225788bd5febaea885bdff86c7bc38399c5b45
Derin <git@dernil.com>
2025-08-17 05:18:00 +0200

    pieces

--- src/torrent.rs
+++ src/torrent.rs
@@ -6,6 +6,9 @@ use std::io;
 pub struct Torrent {
     pub info_hash: [u8; 20],
     pub announce_urls: Vec<String>,
+    pub piece_len: u64,
+    pub piece_hashes: Vec<[u8; 20]>,
+    pub total_size: u64,
 }
 
 impl Torrent {
@@ -117,9 +120,47 @@ impl Torrent {
             }
         }
 
+        let mut piece_length = 0;
+        if let bencoding::Statement::Integer(pl) = &info.get("piece length".as_bytes()).unwrap() {
+            piece_length = *pl;
+            println!("got piece length {piece_length}");
+        }
+
+        let mut pieces = Vec::new();
+        if let bencoding::Statement::ByteString(s) = &info.get("pieces".as_bytes()).unwrap() {
+            if s.len() % 20 != 0 {
+                return Err(easy_err("pieces length is not a multiple of 20"));
+            }
+            for i in 0..s.len() / 20 {
+                pieces.push(s[i..i+20].try_into().unwrap());
+            }
+        }
+
+        println!("got piece hashes {}", pieces.len());
+
+        let mut total_len = 0;
+        if info.contains_key("files".as_bytes()) {
+            if let bencoding::Statement::List(ld) = &info.get("files".as_bytes()).unwrap() {
+                for d in ld {
+                    if let Some(num) = get_file_len(d) {
+                        total_len += num;
+                    }
+                }
+            }
+        } else {
+            if let Some(num) = get_file_len(&statements[0]) {
+                total_len += num;
+            }
+        }
+
+        println!("got total size {total_len}");
+
         let s = Self {
             info_hash: info_hash_bs,
             announce_urls: announce_urls,
+            piece_len: piece_length as u64,
+            total_size: total_len,
+            piece_hashes: pieces,
         };
 
         println!(
@@ -128,10 +169,34 @@ impl Torrent {
             s.get_info_hash_str()
         );
 
+        println!("got total pieces {}", s.get_total_piece_count());
+
+        if s.get_total_piece_count() != s.piece_hashes.len() as u64 {
+            return Err(easy_err("total piece count is not equal to piece hashes length"));
+        }
+
         Ok(s)
+    }
+
+    pub fn get_total_piece_count(self: &Self) -> u64 {
+        (self.total_size as f64 / self.piece_len as f64).ceil() as u64
     }
 
     pub fn get_info_hash_str(self: &Self) -> String {
         percent_encoding::percent_encode(&self.info_hash, NON_ALPHANUMERIC).to_string()
+    }
+}
+
+fn get_file_len(d: &bencoding::Statement) -> Option<u64> {
+    if let bencoding::Statement::Dictionary(dict) = d {
+        dict.get("length".as_bytes()).and_then(|len_stmt| {
+            if let bencoding::Statement::Integer(n) = len_stmt {
+                Some(*n as u64)
+            } else {
+                None
+            }
+        })
+    } else {
+        None
     }
 }
